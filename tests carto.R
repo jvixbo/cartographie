3c02e6eb55b17a8803c61febb58c19fc6141c8e7
BADOL Coline <cbbiologie@gmail.com>
2022-03-11 11:29:25 +0100

    test update

--- tests carto.R
+++ tests carto.R
@@ -17,8 +17,20 @@ mapview(sp_total, zcol = "tot_sp", at = c(0,6,13,24,39))  +
 
 #On va essayer de faire un "focus" sur les régions
 mapview(sp_total, zcol = "tot_sp", at = c(0,6,13,24,39))  +
-  mapview(peche_dir, burst=TRUE, zcol ="Nom_Depa", alpha.regions = 0, legend=FALSE)
+  mapview(peche_dir, burst=TRUE, zcol ="Nom_Depa", alpha.regions = 0.5, legend=FALSE, col.regions=c("blue", "red", "pink", "black"))
 #J'ai mis legend=FALSE sinon on a des couleurs pour chaque région
+#Pas top, on voit que les bassins versants font parfois partie de plusieurs départements ou sortent carrément de Bretagne
 
-#On essaye de faire un popup
-mapview(sp_total, zcol = "tot_sp", popup = popupTable(sp_total, zcol = c("ABH","ABL","ALA")))
+#J'essaye d'ajouter les stations de peche "Wama"
+mapview(sp_total, zcol = "tot_sp", at = c(0,6,13,24,39))  +
+  mapview(wama, col.regions = "white", legend=FALSE)
+#Ce qui serait intéressant, ce serait de faire des points plus ou moins gros en fonction de la population totale dans les stations
+
+#On va essayer de combiner toutes les données de pêche
+mapview(sp_total, zcol = "tot_sp", at = c(0,6,13,24,39))  +
+  mapview(ASPE, col.regions = "white", legend=FALSE) +
+  mapview(AELB, col.regions = "black", legend=FALSE) +
+  mapview(fede, col.regions = "red", legend=FALSE) +
+  mapview(geo15, col.regions = "light blue", legend=FALSE) +
+  mapview(geo18, col.regions = "blue", legend=FALSE) +
+  
--- tests import donnees.R
+++ tests import donnees.R
@@ -1,3 +1,12 @@
+library(sf)
+library(tidyverse)
+library(dplyr)
+library(mapview)
+library(ggplot2)
+library(cartography)
+
+
+
 #Mettre le chemin complet à partir du working directory (sans le mettre dans la liste) sinon ça indique que le fichier n'existe pas
 sp_total<-st_read("Raw Data/ATLAS_POISSONS/ATLAS_SIG/atlas_piscicole_bretagne_20200220/layers/bv_20200220_Especes_totales.shp")
 
@@ -9,10 +18,22 @@ sp_total %>% #ctlr + shift + M pour faire l'opérateur pipe
 #Affiche les titres des colonnes
 names(sp_total)
 
-####################################################
+peche_dir<-st_read("Raw Data/ATLAS_POISSONS/ATLAS_SIG/atlas_piscicole_bretagne_20200220/layers/dir_compte_peche.shp")
 
 exu<-st_read("Raw Data/ATLAS_POISSONS/donnees_geographiques_reference/exutoires_BV_20200128.shp")
 
 peche_dir %>% 
   st_drop_geometry() %>% 
   View()
+
+wama<-st_read("Raw Data/ATLAS_POISSONS/donnees_geographiques_reference/peches_WAMA_BZH_1978_2019_20200215.shp")
+
+AELB<-st_read("Raw Data/ATLAS_POISSONS/donnees_geographiques_reference/peches_AELB_BZH_2016_2018_20200215.shp")
+
+fede<-st_read("Raw Data/ATLAS_POISSONS/donnees_geographiques_reference/peche_fede_56_20200215.shp")
+
+geo15<-st_read("Raw Data/ATLAS_POISSONS/donnees_geographiques_reference/peche_georect_sd_2015_2019_20200215.shp")
+
+geo18<-st_read("Raw Data/ATLAS_POISSONS/donnees_geographiques_reference/peche_georect_sd_2015_2019_20210818.shp")
+
+ASPE<-st_read("Raw Data/ATLAS_POISSONS/donnees_geographiques_reference/peche_ASPE_20200215.shp")
